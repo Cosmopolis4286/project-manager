1a2fa9bed0ee8e1d6370da5a5a034c44ce684405
Cosmopolis4286 <83352571+Cosmopolis4286@users.noreply.github.com>
2026-01-19 18:31:15 -0300

    feat: adicionar método de permissão para reordenar projetos na ProjectPolicy
    - Incluído método reorder para validar se o usuário é proprietário de todos os projetos envolvidos na reordenação
    - Mantidos os demais métodos e comentários com melhorias na documentação (PHPDoc)

--- src/app/Policies/ProjectPolicy.php
+++ src/app/Policies/ProjectPolicy.php
@@ -9,6 +9,10 @@ class ProjectPolicy
 {
     /**
      * Verifica se o projeto pertence ao usuário autenticado.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
      */
     public function owner(User $user, Project $project): bool
     {
@@ -17,6 +21,10 @@ class ProjectPolicy
 
     /**
      * Determine se o usuário pode visualizar o projeto.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
      */
     public function view(User $user, Project $project): bool
     {
@@ -25,6 +33,10 @@ class ProjectPolicy
 
     /**
      * Determine se o usuário pode atualizar o projeto.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
      */
     public function update(User $user, Project $project): bool
     {
@@ -33,6 +45,10 @@ class ProjectPolicy
 
     /**
      * Determine se o usuário pode deletar o projeto.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
      */
     public function delete(User $user, Project $project): bool
     {
@@ -41,6 +57,10 @@ class ProjectPolicy
 
     /**
      * Determine se o usuário pode restaurar o projeto.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
      */
     public function restore(User $user, Project $project): bool
     {
@@ -49,6 +69,10 @@ class ProjectPolicy
 
     /**
      * Determine se o usuário pode deletar permanentemente o projeto.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
      */
     public function forceDelete(User $user, Project $project): bool
     {
@@ -58,6 +82,9 @@ class ProjectPolicy
     /**
      * Determine se o usuário pode criar um projeto.
      * Geralmente qualquer usuário autenticado pode criar.
+     *
+     * @param User $user
+     * @return bool
      */
     public function create(User $user): bool
     {
@@ -66,9 +93,29 @@ class ProjectPolicy
 
     /**
      * Pode listar projetos? Permitido para qualquer usuário autenticado.
+     *
+     * @param User $user
+     * @return bool
      */
     public function viewAny(User $user): bool
     {
+        return true;
+    }
+
+    /**
+     * Determine se o usuário pode reordenar os projetos.
+     *
+     * @param User $user
+     * @param iterable|array $projects
+     * @return bool
+     */
+    public function reorder(User $user, iterable $projects): bool
+    {
+        foreach ($projects as $project) {
+            if (!$this->owner($user, $project)) {
+                return false;
+            }
+        }
         return true;
     }
 }
